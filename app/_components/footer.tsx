d3ee97eb27dcaf63c1f827f357f9bff8f811d768
Rohit <152020190+Rohiyadaop@users.noreply.github.com>
2026-07-22 19:22:31 +0530

    Footer change

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div className="container mx-auto px-4 py-8 lg:px-20 xl:px-32">
                     <div className="flex flex-col md:flex-row justify-between items-center">
                         <div className="text-center md:text-left mb-4 md:mb-0">
-                            <p className="text-muted-foreground">© 2024 Alex Developer. All rights reserved.</p>
+                            <p className="text-muted-foreground">2026 ROHIT YADAV.  All rights reserved.</p>
                         </div>
                         <div className="flex space-x-4">
                             <Button variant="ghost" size="icon" asChild>
